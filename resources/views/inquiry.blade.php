b9c5ce9a110eeb4188fc40d4a15d4e725c4bf36f
Ufaa <dhuwanee.no1@gmail.com>
2021-12-06 21:29:15 +0900

    20211206-3

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -4,28 +4,25 @@
     color: black;
     padding: 5px 40px;
     border: none;
-  }
-
-  tr:nth-child(4) td {
-    padding: 10px;
+    text-align: left;
   }
 
   td {
-    padding: 25px 40px;
-    text-align: center;
+    padding: 5px 40px;
   }
 
   input {
     padding: 5px;
   }
 
-  .btn btn-primary {
+  .btn {
     padding: 10px 20px;
-    background-color: #289ADC;
+    background-color: black;
     border: none;
-    color: black;
+    color: white;
     padding: 5px 40px;
     border-radius: 5px;
+    margin-left: 200px;
   }
 
   span {
@@ -54,60 +51,80 @@
         お名前<span>※</span>
       </th>
       <td>
-        <input type="text" name="fullname" value="{{ old('fullname') }}" style="width:675px;" />
+        <input type="text" name="fullname" value="{{ old('fullname') }}" style="width:675px; height:40px;" />
       </td>
+    </tr>
+    <tr>
+      <th></th>
+      <td>例)山田 太郎</td>
     </tr>
     <tr>
       <th>
         性別<span>※</span>
       </th>
       <td>
-        <input type="text" name="gender" value="{{ old('gender') }}" style="width:675px;" />
+        <input type="text" name="gender" value="{{ old('gender') }}" style="width:675px; height:40px;" />
       </td>
+    </tr>
+    <tr>
+      <th></th>
+      <td>0:男性、1:女性</td>
     </tr>
     <tr>
       <th>
         メールアドレス<span>※</span>
       </th>
       <td>
-        <input type="text" name="email" value="{{ old('email') }}" style="width:675px;" />
+        <input type="text" name="email" value="{{ old('email') }}" style="width:675px; height:40px;" />
       </td>
     </tr>
     <tr>
+      <th></th>
+      <td>例)test@example.com</td>
+    </tr>
     <tr>
       <th>
         郵便番号<span>※</span>
       </th>
       <td>
         〒
-        <input type="text" name="postcode" value="{{ old('postcode') }}" style="width:675px;" />
+        <input type="text" name="postcode" value="{{ old('postcode') }}" style="width:655px; height:40px;" />
       </td>
     </tr>
     <tr>
+      <th></th>
+      <td>例)123-4567</td>
+    </tr>
     <tr>
       <th>
         住所<span>※</span>
       </th>
       <td>
-        <input type="text" name="address" value="{{ old('address') }}" style="width:675px;" />
+        <input type="text" name="address" value="{{ old('address') }}" style="width:675px; height:50px;" />
       </td>
     </tr>
     <tr>
+      <th></th>
+      <td>例)東京都渋谷区千駄ヶ谷1-2-3</td>
+    </tr>
     <tr>
       <th>
         建物名
       </th>
       <td>
-        <input type="text" name="building_name" value="{{ old('building_name') }}" style="width:675px;" />
+        <input type="text" name="building_name" value="{{ old('building_name') }}" style="width:675px; height:40px;" />
       </td>
     </tr>
     <tr>
+      <th></th>
+      <td>例)千駄ヶ谷マンション101</td>
+    </tr>
     <tr>
       <th>
         ご意見<span>※</span>
       </th>
       <td>
-        <input type=" text" name="opinion" value="{{ old('opinion') }}" style="width:675px;" />
+        <input type=" text" name="opinion" value="{{ old('opinion') }}" style="width:675px; height:200px;" />
       </td>
     </tr>
     <tr>
